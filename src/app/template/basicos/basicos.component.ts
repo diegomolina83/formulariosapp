34d1f5cb1e134a8d1bf2c1aa3057834ef3b5075b
Diego <diego.molina@creamteam.de>
2021-10-15 14:22:09 +0200

    commit15102021

--- src/app/template/basicos/basicos.component.ts
+++ src/app/template/basicos/basicos.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import {NgForm } from '@angular/forms'
 
 @Component({
   selector: 'app-basicos',
@@ -8,9 +9,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BasicosComponent implements OnInit {
 
+@ViewChild('miFormulario') miFormulario!: NgForm;
+
   constructor() { }
 
   ngOnInit(): void {
   }
+
+nombreValido():boolean{
+return this.miFormulario?.controls.producto?.invalid && this.miFormulario?.controls.producto?.touched;
+}
+
+precioValido():boolean{
+  return (this.miFormulario?.controls.precio?.value <0) && this.miFormulario?.controls.precio?.touched;
+}
+
+guardar(){
+  console.log(this.miFormulario.value);
+  console.log("Valid : ", this.miFormulario.valid);
+  console.log(this.miFormulario)
+  
+}
 
 }
