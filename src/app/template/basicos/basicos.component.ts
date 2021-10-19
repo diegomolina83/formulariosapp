86fcbdb89ea21eea331553d4b9f5be951cf9df00
Diego <diego.molina@creamteam.de>
2021-10-19 11:25:55 +0200

    dinamic form

--- src/app/template/basicos/basicos.component.ts
+++ src/app/template/basicos/basicos.component.ts
@@ -1,34 +1,45 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
-import {NgForm } from '@angular/forms'
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-basicos',
   templateUrl: './basicos.component.html',
-  styles: [
-  ]
+  styles: [],
 })
 export class BasicosComponent implements OnInit {
+  @ViewChild('miFormulario') miFormulario!: NgForm;
 
-@ViewChild('miFormulario') miFormulario!: NgForm;
+  initForm = {
+    producto: 'RTX 3090',
+    precio: 1500,
+    existencias: 10,
+  };
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  nombreValido(): boolean {
+    return (
+      this.miFormulario?.controls.producto?.invalid &&
+      this.miFormulario?.controls.producto?.touched
+    );
   }
 
-nombreValido():boolean{
-return this.miFormulario?.controls.producto?.invalid && this.miFormulario?.controls.producto?.touched;
-}
+  precioValido(): boolean {
+    return (
+      this.miFormulario?.controls.precio?.value < 0 &&
+      this.miFormulario?.controls.precio?.touched
+    );
+  }
 
-precioValido():boolean{
-  return (this.miFormulario?.controls.precio?.value <0) && this.miFormulario?.controls.precio?.touched;
-}
-
-guardar(){
-  console.log(this.miFormulario.value);
-  console.log("Valid : ", this.miFormulario.valid);
-  console.log(this.miFormulario)
-  
-}
-
+  guardar() {
+    console.log(this.miFormulario.value);
+    console.log('Valid : ', this.miFormulario.valid);
+    console.log(this.miFormulario);
+    this.miFormulario.resetForm({
+      precio: 0,
+      existencias: 1,
+    });
+  }
 }
